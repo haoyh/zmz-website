e4bea434df782f6d1c6c994d3fc994d5d3447fd1
郝艳辉 <buzhiming_hao@163.com>
2021-10-13 18:34:21 +0800

    package;

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === "production" ? "/zmz-website/" : "/",
-  publicPath: "./",
+  publicPath: process.env.NODE_ENV === "production" ? "/zmz-website/" : "/",
+  // publicPath: "./",
 };
